3763bfccf31d1100944777da7fb1b351a31f1287
Lukas Snoek <lukassnoek@gmail.com>
2016-09-02 17:54:14 +0200

    Add concat_iterables function

--- spynoza/nodes/__init__.py
+++ spynoza/nodes/__init__.py
@@ -1,4 +1,5 @@
 from .filtering import apply_sg_filter
-from .utils import get_scaninfo, dyns_min_1, topup_scan_params, apply_scan_params
+from .utils import get_scaninfo, dyns_min_1, topup_scan_params, apply_scan_params, concat_iterables
 
-__all__ = ['apply_sg_filter', 'get_scaninfo', 'dyns_min_1', 'topup_scan_params', 'apply_scan_params']
+__all__ = ['apply_sg_filter', 'get_scaninfo', 'dyns_min_1', 'topup_scan_params', 'apply_scan_params',
+           'concat_iterables']
