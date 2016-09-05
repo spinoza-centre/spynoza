908662660a4abc46373872b1a35bb763d84ace74
Lukas Snoek <lukassnoek@gmail.com>
2016-09-05 18:08:11 +0200

    Add top-level imports

--- spynoza/workflows/__init__.py
+++ spynoza/workflows/__init__.py
@@ -1,7 +1,9 @@
 from .motion_correction import create_motion_correction_workflow
 from .registration import create_registration_workflow
 from .topup import create_topup_workflow
+from .all_3T import create_all_3T_workflow
+from .all_7T import create_all_7T_workflow
 
-__all__ = ['create_motion_correction_workflow',
-            'create_registration_workflow',
-            'create_topup_workflow']
+
+__all__ = ['create_motion_correction_workflow', 'create_registration_workflow',
+           'create_topup_workflow', 'create_all_3T_workflow', 'create_all_7T_workflow']
