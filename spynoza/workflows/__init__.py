6528f3af4dc42271a0118efe8c2b66dd102155d2
Lukas Snoek <lukassnoek@gmail.com>
2017-02-16 18:19:04 +0100

    Misc changes

--- spynoza/workflows/__init__.py
+++ spynoza/workflows/__init__.py
@@ -4,7 +4,9 @@ from .topup_unwarping import create_topup_workflow
 from .B0_unwarping import create_B0_workflow
 from .all_3T import create_all_3T_workflow
 from .all_7T import create_all_7T_workflow
-from .ica_fix import create_ica_workflow
+from .ica_fix import create_melodic_workflow
 
 __all__ = ['create_motion_correction_workflow', 'create_registration_workflow',
-           'create_topup_workflow', 'create_B0_workflow', 'create_all_3T_workflow', 'create_all_7T_workflow']
+           'create_topup_workflow', 'create_B0_workflow',
+           'create_all_3T_workflow', 'create_all_7T_workflow',
+           'create_melodic_workflow']
