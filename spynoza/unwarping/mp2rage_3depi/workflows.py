de94f9e64a3bc5dc98188a2f2af981a9a63dfe20
Gilles de Hollander <gilles.de.hollander@gmail.com>
2017-09-01 17:52:42 +0200

    Allow for Freesurfer init for MP2RAGE_3dEPI

--- spynoza/unwarping/mp2rage_3depi/workflows.py
+++ spynoza/unwarping/mp2rage_3depi/workflows.py
@@ -3,9 +3,14 @@ import nipype.interfaces.utility as util
 from nipype.interfaces import fsl
 from nipype.interfaces import afni
 from nipype.interfaces import ants
+from nipype.interfaces import freesurfer
+from nipype.interfaces.base import traits
 import pkg_resources
+from nipype.interfaces.c3 import C3dAffineTool
 
-def create_3DEPI_registration_workflow(name='3d_epi_registration'):
+
+def create_3DEPI_registration_workflow(name='3d_epi_registration',
+                                       init_reg_file=None):
     
     workflow = pe.Workflow(name=name)
     
@@ -25,8 +30,30 @@ def create_3DEPI_registration_workflow(name='3d_epi_registration'):
     workflow.connect(automask, 'out_file', mask_T1_EPI, 'mask_file')
     
     
-    json = pkg_resources.resource_filename('niworkflows.data', 't1-mni_registration_testing_002.json')
-    reg = pe.Node(ants.Registration(from_file=json), name='registration')
+    json = pkg_resources.resource_filename('niworkflows.data', 't1-mni_registration_testing_000.json')
+
+    if init_reg_file is not None:
+        reg = pe.Node(ants.Registration(from_file=json), name='registration')
+        
+        if init_reg_file.endswith('lta'):
+            convert_to_mat = pe.Node(freesurfer.utils.LTAConvert(in_lta=init_reg_file,
+                                                                 out_fsl=True), name='convert_to_mat')
+            workflow.connect(inputnode, 'T1_EPI_file', convert_to_mat, 'source_file')
+#             workflow.connect(inputnode, 'T1_file', convert_to_mat, 'target_file')
+            
+            convert_to_ants = pe.Node(C3dAffineTool(fsl2ras=True,
+                                                   itk_transform=True), name='convert_to_ants')
+            workflow.connect(inputnode, 'T1_EPI_file', convert_to_ants, 'source_file')
+            workflow.connect(inputnode, 'T1_file', convert_to_ants, 'reference_file')
+            workflow.connect(convert_to_mat, 'out_fsl', convert_to_ants, 'transform_file')            
+            
+            workflow.connect(convert_to_ants, 'itk_transform', reg, 'initial_moving_transform')
+            
+        else:
+            reg.inputs.initial_moving_transform = init_reg_file
+
+    else:
+        reg = pe.Node(ants.Registration(from_file=json), name='registration')        
     
     workflow.connect(mask_T1_EPI, 'out_file', reg, 'moving_image')
     workflow.connect(inputnode, 'T1_file', reg, 'fixed_image')
