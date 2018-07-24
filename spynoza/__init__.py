504ec6fb1952c939b089e2658c18fc7b04316cad
Tomas Knapen <tknapen@gmail.com>
2018-07-24 10:07:11 +0200

    added conversions to imports from __init__

--- spynoza/__init__.py
+++ spynoza/__init__.py
@@ -1,8 +1,9 @@
-__version__ = '0.0.1'
+__version__ = '0.0.1b'
 
 import os.path as op
 
 from . import denoising
+from . import conversion
 from . import filtering
 from . import glm
 from . import ica_fix
@@ -15,5 +16,5 @@ root_dir = op.dirname(op.abspath(__file__))
 test_data_path = op.join(root_dir, 'data', 'test_data')
 
 __all__ = ['unwarping', 'uniformization', 'registration',
-           'retroicor', 'masking', 'ica_fix', 'glm',
+           'retroicor', 'masking', 'ica_fix', 'glm', 'conversion'
            'filtering', 'test_data_path', 'root_dir']
